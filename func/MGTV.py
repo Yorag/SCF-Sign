b6320348452d97f372b82af2d92db5306b9bfced
Delay-L <lkw0134@163.com>
2020-07-27 12:19:40 +0800

    芒果TV

--- func/MGTV.py
+++ func/MGTV.py
@@ -10,41 +10,92 @@ class MGTV:
     芒果签到
     *奖励：连签21天，得15天体验会员；对应积分
     '''
-    def __init__(self, params):
+    def __init__(self, uuid, ticket):
         '''
         初始化
-        :param params: url关键字credits.bz.mgtv.com/user/creditsTake的?后所有参数
+        :param uuid: uuid
+        :param ticket: ticket
         '''
-        self.params = params
-        self.params["timestamp"] = round(time.time())
+        self.uuid = uuid
+        self.ticket = ticket
+        self.urls = {
+            "APP_SIGN": "https://credits.bz.mgtv.com/user/creditsTake",
+            "PC_SIGN": "https://task.bz.mgtv.com/user/task_take",
+            "WR_COMMENT": "https://task.bz.mgtv.com/credits/toast"
+        }
 
 
-    def sign(self):
+    def sign_app(self):
         '''
         签到
         :return: 返回信息
         '''
-        url = "https://credits.bz.mgtv.com/user/creditsTake"
-        res = requests.get(url, params=self.params)
+        params = {
+            "type": 1,
+            "uuid": self.uuid,
+            "ticket": self.ticket
+        }
+        res = requests.get(self.urls["APP_SIGN"], params=params)
         res_json = json.loads(res.content.decode().replace("__jp5(", "").replace(");", ""))
-        print("（mgtv）签到信息", res.content.decode())
+        print("（mgtv）签到信息(1)", res.content.decode())
         if res_json["code"] == 200:
             curDay = res_json["data"]["curDay"]
             credits = res_json["data"]["credits"]
-            msg = f"签到：+{credits}积分\n已签到：{curDay}天/21天"
+            msg = f"+{credits}积分\n已签到：{curDay}天/21天"
         else:
             msg = "签到失败"
+        return msg
+
+
+    def sign_web(self):
+        """
+        签到网页端
+        :return: 返回信息
+        """
+        params = {
+            "type": 5,
+            "uuid": self.uuid,
+            "ticket": self.ticket,
+            "platform": "4"
+        }
+        res = requests.get(self.urls["PC_SIGN"], params=params)
+        print("（mgtv）签到信息(2)", res.content.decode())
+        if res.json()["code"] == 200:
+            msg = res.json()["msg"]
+        else:
+            msg = "签到失败"
+        return msg
+
+
+    def task(self):
+        """
+        评论任务
+        :return: 返回信息
+        """
+        params = {
+            "type": 5,
+            "uuid": self.uuid,
+            "ticket": self.ticket,
+            "platform": 4
+        }
+        # 评论
+        res = requests.get(self.urls["WR_COMMENT"], params=params)
+        # 领取奖励
+        res = requests.get(self.urls["PC_SIGN"], params=params)
+        print("（mgtv）评论领奖信息", res.content.decode())
+        if res.json()["code"] == 200:
+            msg = res.json()["msg"]
+        else:
+            msg = "领奖失败"
         return msg
 
 
 
 
 if __name__ == '__main__':
-    params = {
-        "uuid": "",
-        "ticket": "",
-        "type": 1
-    }
-    obj = MGTV(params)
-    msg = obj.sign()
-    print("【芒果tv签到】", msg)
+    uuid = ""
+    ticket = ""
+
+    obj = MGTV(uuid, ticket)
+    msg = f"签到(1)：{obj.sign_app()}\n签到(2)：{obj.sign_web()}\n任务：{obj.task()}"
+    print("【芒果TV】", msg)
